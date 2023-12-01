c39019c5e3eb88e8c28057bdba1b3c30e25edf12
Pham Cong Vinh <vvvvvvvvvvvvvvvinh@gmail.com>
2023-12-01 13:59:40 +0700

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,41 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter-2",
+  "level": "1",
+  "url": "frontmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  My Website   copyright  "
+},
+{
+  "id": "sec-section-name",
+  "level": "1",
+  "url": "sec-section-name.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Section Title",
+  "body": " Section Title  Text of section.  "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
