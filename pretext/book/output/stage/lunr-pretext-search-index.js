ec6ec7dc28420760a13ecea896a12ccce121a30e
Pham Cong Vinh <vvvvvvvvvvvvvvvinh@gmail.com>
2023-12-08 22:53:29 +0700

    Re-add the book
    Re-add the book
    
    Hoping the submodule problem doesn't persist

---
+++ pretext/book/output/stage/lunr-pretext-search-index.js
@@ -0,0 +1,248 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter-2",
+  "level": "1",
+  "url": "frontmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  My Website   copyright  "
+},
+{
+  "id": "sec-so-thuc-va-cac-tinh-chat",
+  "level": "1",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Số Thực Và Các Tính Chất",
+  "body": " Số Thực Và Các Tính Chất  Text of section.  "
+},
+{
+  "id": "sec-vector-cot",
+  "level": "1",
+  "url": "sec-vector-cot.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Vector Cột",
+  "body": " Vector Cột  Text of section.  "
+},
+{
+  "id": "sec-khong-gian-vector",
+  "level": "1",
+  "url": "sec-khong-gian-vector.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Không Gian Vector",
+  "body": " Không Gian Vector  Text of section.  "
+},
+{
+  "id": "sec-ma-tran",
+  "level": "1",
+  "url": "sec-ma-tran.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Ma Trận",
+  "body": " Ma Trận  Text of section.  "
+},
+{
+  "id": "sec-cac-ma-tran-co-so",
+  "level": "1",
+  "url": "sec-cac-ma-tran-co-so.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Các Ma Trận Cơ Sở",
+  "body": " Các Ma Trận Cơ Sở  Text of section.  "
+},
+{
+  "id": "sec-cac-phep-bien-doi-hang",
+  "level": "1",
+  "url": "sec-cac-phep-bien-doi-hang.html",
+  "type": "Section",
+  "number": "2.3",
+  "title": "Các Phép Biến Đổi Hàng",
+  "body": " Các Phép Biến Đổi Hàng  Text of section.  "
+},
+{
+  "id": "sec-ma-tran-hoan-vi",
+  "level": "1",
+  "url": "sec-ma-tran-hoan-vi.html",
+  "type": "Section",
+  "number": "2.4",
+  "title": "Ma Trận Hoán Vị",
+  "body": " Ma Trận Hoán Vị  Text of section.  "
+},
+{
+  "id": "sec-ma-tran-bac-thang",
+  "level": "1",
+  "url": "sec-ma-tran-bac-thang.html",
+  "type": "Section",
+  "number": "2.5",
+  "title": "Ma Trận Bậc Thang",
+  "body": " Ma Trận Bậc Thang  Text of section.  "
+},
+{
+  "id": "sec-tru-cua-hang-va-hang-cua-ma-tran",
+  "level": "1",
+  "url": "sec-tru-cua-hang-va-hang-cua-ma-tran.html",
+  "type": "Section",
+  "number": "2.6",
+  "title": "Trụ Của Hàng Và Hạng Của Ma Trận",
+  "body": " Trụ Của Hàng Và Hạng Của Ma Trận  Text of section.  "
+},
+{
+  "id": "sec-tong-cua-hai-ma-tran",
+  "level": "1",
+  "url": "sec-tong-cua-hai-ma-tran.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Tổng Của Hai Ma Trận",
+  "body": " Tổng Của Hai Ma Trận  Text of section.  "
+},
+{
+  "id": "sec-nhan-ma-tran-voi-mot-so",
+  "level": "1",
+  "url": "sec-nhan-ma-tran-voi-mot-so.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Nhân Ma Trận Với Một Số",
+  "body": " Nhân Ma Trận Với Một Số  Text of section.  "
+},
+{
+  "id": "sec-nhan-hai-ma-tran",
+  "level": "1",
+  "url": "sec-nhan-hai-ma-tran.html",
+  "type": "Section",
+  "number": "3.3",
+  "title": "Nhân Hai Ma Trận",
+  "body": " Nhân Hai Ma Trận  Text of section.  "
+},
+{
+  "id": "sec-ma-tran-khoi",
+  "level": "1",
+  "url": "sec-ma-tran-khoi.html",
+  "type": "Section",
+  "number": "3.4",
+  "title": "Ma Trận Khối",
+  "body": " Ma Trận Khối  Text of section.  "
+},
+{
+  "id": "sec-nhan-ma-tran-theo-khoi",
+  "level": "1",
+  "url": "sec-nhan-ma-tran-theo-khoi.html",
+  "type": "Section",
+  "number": "3.5",
+  "title": "Nhân Ma Trận Theo Khối",
+  "body": " Nhân Ma Trận Theo Khối  Text of section.  "
+},
+{
+  "id": "sec-dinh-thuc",
+  "level": "1",
+  "url": "sec-dinh-thuc.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Định Thức",
+  "body": " Định Thức  Text of section.  "
+},
+{
+  "id": "sec-ma-tran-con-va-phan-phu-dai-so",
+  "level": "1",
+  "url": "sec-ma-tran-con-va-phan-phu-dai-so.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Ma Trận Con Và Phần Phụ Đại Số",
+  "body": " Ma Trận Con Và Phần Phụ Đại Số  Text of section.  "
+},
+{
+  "id": "sec-tinh-dinh-thuc",
+  "level": "1",
+  "url": "sec-tinh-dinh-thuc.html",
+  "type": "Section",
+  "number": "4.3",
+  "title": "Tính Định Thức",
+  "body": " Tính Định Thức  Text of section.  "
+},
+{
+  "id": "sec-phuong-phap-khu-gauss",
+  "level": "1",
+  "url": "sec-phuong-phap-khu-gauss.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Phương Pháp Khử Gauss",
+  "body": " Phương Pháp Khử Gauss  Text of section.  "
+},
+{
+  "id": "sec-phuong-phap-cramer-doi-voi-he-phuong-trinh-co-so-phuong-trinh-bang-so-an",
+  "level": "1",
+  "url": "sec-phuong-phap-cramer-doi-voi-he-phuong-trinh-co-so-phuong-trinh-bang-so-an.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Phương Pháp Cramer Đối Với Hệ Phương Trình Có Số Phương Trình Bằng Số Ẩn",
+  "body": " Phương Pháp Cramer Đối Với Hệ Phương Trình Có Số Phương Trình Bằng Số Ẩn  Text of section.  "
+},
+{
+  "id": "sec-mot-so-ung-dung",
+  "level": "1",
+  "url": "sec-mot-so-ung-dung.html",
+  "type": "Section",
+  "number": "5.3",
+  "title": "Một Số Ứng Dụng",
+  "body": " Một Số Ứng Dụng  Text of section.  "
+},
+{
+  "id": "sec-phan-tich-a-lu",
+  "level": "1",
+  "url": "sec-phan-tich-a-lu.html",
+  "type": "Section",
+  "number": "6.1",
+  "title": "Phân Tích A = LU",
+  "body": " Phân Tích A = LU  Text of section.  "
+},
+{
+  "id": "sec-phan-tich-a-cr",
+  "level": "1",
+  "url": "sec-phan-tich-a-cr.html",
+  "type": "Section",
+  "number": "6.2",
+  "title": "Phân Tích A = CR",
+  "body": " Phân Tích A = CR  Text of section.  "
+},
+{
+  "id": "sec-ma-tran-nghich-dao",
+  "level": "1",
+  "url": "sec-ma-tran-nghich-dao.html",
+  "type": "Section",
+  "number": "6.3",
+  "title": "Ma Trận Nghịch Đảo",
+  "body": " Ma Trận Nghịch Đảo  Text of section.  "
+},
+{
+  "id": "sec-ung-dung-de-giai-he-phuong-trinh-tuyen-tinh",
+  "level": "1",
+  "url": "sec-ung-dung-de-giai-he-phuong-trinh-tuyen-tinh.html",
+  "type": "Section",
+  "number": "6.4",
+  "title": "Ứng Dụng Để Giải Hệ Phương Trình Tuyến Tính",
+  "body": " Ứng Dụng Để Giải Hệ Phương Trình Tuyến Tính  Text of section.  "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
---
+++ pretext/book/output/web/lunr-pretext-search-index.js
@@ -0,0 +1,392 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter-2",
+  "level": "1",
+  "url": "frontmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  My Website   copyright  "
+},
+{
+  "id": "sec-so-thuc-va-cac-tinh-chat",
+  "level": "1",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Số Thực Và Các Tính Chất",
+  "body": " Số Thực Và Các Tính Chất   Tập Số Thực   Tập số tự nhiên (hay tập số nguyên không âm), kí hiệu     Tập số tự nhiên dương , kí hiệu .    Tập số nguyên , kí hiệu     Tập số hữu tỷ , kí hiệu . Số hữu tỷ (biểu diễn dưới dạng thập phân) có dạng hữu hạn hoặc vô hạn tuần hoàn .  Ví dụ:    Tập số vô tỷ , kí hiệu , là tập các số thập phân vô hạn không tuần hoàn.   Ví dụ:    Tập số thực , kí hiệu , là tập bao gồm các số hữu tỷ và các số vô tỷ.     Các Phép Toán Trên Tập Số Thực  Với 2 số  Phép cộng  Cộng 2 số thực ta được 1 số thực , gọi là tổng của .  Ký hiệu:   Phép nhân  Nhân 2 số thực ta được 1 số thực , gọi là tích của .  Ký hiệu:     Tính Chất Của Các Phép Toán   Phép cộng có các tính chất:  Giao hoán:  Kết hợp:  Số :  Số đối:    Phép nhân có các tính chất:  Giao hoán:  Kết hợp:  Số :    Tính chất phân phối của 2 phép toán:    Tính chất giao hoán  Do tính giao hoán và , ta có:     Tính phân phối và giao hoán      Tính đóng của 1 phép toán  Với mọi , khi:     Tổng , ta nói phép cộng các số thực có tính đóng.    Tích , ta nói phép nhân các số thực có tính đóng.      Sự phụ thuộc của tính đóng với tập số  Với phép cộng và phép nhân, tính đóng phụ thuộc vào tập chứa các phần tử mà ta thực hiện phép toán.   Tính đóng đối với tập số nguyên âm  Trên tập các số nguyên âm    Phép cộng có tính đóng.   Phép nhân không có tính đóng.         Sự quan trọng của tính đóng  Tính đóng đối với phép toán có mối quan hệ mật thiết với một đặc tính khái niệm của không gian véc tơ .    "
+},
+{
+  "id": "subsec-tap-so-thuc-2",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-tap-so-thuc-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Tập số tự nhiên "
+},
+{
+  "id": "subsec-tap-so-thuc-3",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-tap-so-thuc-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Tập số tự nhiên dương "
+},
+{
+  "id": "subsec-tap-so-thuc-4",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-tap-so-thuc-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Tập số nguyên "
+},
+{
+  "id": "subsec-tap-so-thuc-5",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-tap-so-thuc-5",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Tập số hữu tỷ "
+},
+{
+  "id": "subsec-tap-so-thuc-7",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-tap-so-thuc-7",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Tập số vô tỷ "
+},
+{
+  "id": "subsec-tap-so-thuc-9",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-tap-so-thuc-9",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Tập số thực "
+},
+{
+  "id": "subsec-cac-phep-toan-tren-tap-so-thuc-3",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-cac-phep-toan-tren-tap-so-thuc-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Phép cộng "
+},
+{
+  "id": "subsec-cac-phep-toan-tren-tap-so-thuc-6",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-cac-phep-toan-tren-tap-so-thuc-6",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Phép nhân "
+},
+{
+  "id": "subsec-tinh-chat-cua-cac-phep-toan-3",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-tinh-chat-cua-cac-phep-toan-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Phép cộng "
+},
+{
+  "id": "subsec-tinh-chat-cua-cac-phep-toan-4",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-tinh-chat-cua-cac-phep-toan-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Phép nhân "
+},
+{
+  "id": "subsec-tinh-chat-cua-cac-phep-toan-5",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-tinh-chat-cua-cac-phep-toan-5",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Tính chất phân phối "
+},
+{
+  "id": "subsec-tinh-chat-cua-cac-phep-toan-6",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-tinh-chat-cua-cac-phep-toan-6",
+  "type": "Observation",
+  "number": "1.1.1",
+  "title": "Tính chất giao hoán.",
+  "body": " Tính chất giao hoán  Do tính giao hoán và , ta có:   "
+},
+{
+  "id": "subsec-tinh-chat-cua-cac-phep-toan-7",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-tinh-chat-cua-cac-phep-toan-7",
+  "type": "Example",
+  "number": "1.1.2",
+  "title": "Tính phân phối và giao hoán.",
+  "body": " Tính phân phối và giao hoán     "
+},
+{
+  "id": "subsec-tinh-chat-cua-cac-phep-toan-8",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-tinh-chat-cua-cac-phep-toan-8",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Tính đóng "
+},
+{
+  "id": "subsec-tinh-chat-cua-cac-phep-toan-11",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-tinh-chat-cua-cac-phep-toan-11",
+  "type": "Note",
+  "number": "1.1.3",
+  "title": "Sự phụ thuộc của tính đóng với tập số.",
+  "body": " Sự phụ thuộc của tính đóng với tập số  Với phép cộng và phép nhân, tính đóng phụ thuộc vào tập chứa các phần tử mà ta thực hiện phép toán.   Tính đóng đối với tập số nguyên âm  Trên tập các số nguyên âm    Phép cộng có tính đóng.   Phép nhân không có tính đóng.       "
+},
+{
+  "id": "subsec-tinh-chat-cua-cac-phep-toan-12",
+  "level": "2",
+  "url": "sec-so-thuc-va-cac-tinh-chat.html#subsec-tinh-chat-cua-cac-phep-toan-12",
+  "type": "Remark",
+  "number": "1.1.5",
+  "title": "Sự quan trọng của tính đóng.",
+  "body": " Sự quan trọng của tính đóng  Tính đóng đối với phép toán có mối quan hệ mật thiết với một đặc tính khái niệm của không gian véc tơ .  "
+},
+{
+  "id": "sec-vector-cot",
+  "level": "1",
+  "url": "sec-vector-cot.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Vector Cột",
+  "body": " Vector Cột  Text of section.  "
+},
+{
+  "id": "sec-khong-gian-vector",
+  "level": "1",
+  "url": "sec-khong-gian-vector.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Không Gian Vector",
+  "body": " Không Gian Vector  Text of section.  "
+},
+{
+  "id": "sec-ma-tran",
+  "level": "1",
+  "url": "sec-ma-tran.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Ma Trận",
+  "body": " Ma Trận  Text of section.  "
+},
+{
+  "id": "sec-cac-ma-tran-co-so",
+  "level": "1",
+  "url": "sec-cac-ma-tran-co-so.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Các Ma Trận Cơ Sở",
+  "body": " Các Ma Trận Cơ Sở  Text of section.  "
+},
+{
+  "id": "sec-cac-phep-bien-doi-hang",
+  "level": "1",
+  "url": "sec-cac-phep-bien-doi-hang.html",
+  "type": "Section",
+  "number": "2.3",
+  "title": "Các Phép Biến Đổi Hàng",
+  "body": " Các Phép Biến Đổi Hàng  Text of section.  "
+},
+{
+  "id": "sec-ma-tran-hoan-vi",
+  "level": "1",
+  "url": "sec-ma-tran-hoan-vi.html",
+  "type": "Section",
+  "number": "2.4",
+  "title": "Ma Trận Hoán Vị",
+  "body": " Ma Trận Hoán Vị  Text of section.  "
+},
+{
+  "id": "sec-ma-tran-bac-thang",
+  "level": "1",
+  "url": "sec-ma-tran-bac-thang.html",
+  "type": "Section",
+  "number": "2.5",
+  "title": "Ma Trận Bậc Thang",
+  "body": " Ma Trận Bậc Thang  Text of section.  "
+},
+{
+  "id": "sec-tru-cua-hang-va-hang-cua-ma-tran",
+  "level": "1",
+  "url": "sec-tru-cua-hang-va-hang-cua-ma-tran.html",
+  "type": "Section",
+  "number": "2.6",
+  "title": "Trụ Của Hàng Và Hạng Của Ma Trận",
+  "body": " Trụ Của Hàng Và Hạng Của Ma Trận  Text of section.  "
+},
+{
+  "id": "sec-tong-cua-hai-ma-tran",
+  "level": "1",
+  "url": "sec-tong-cua-hai-ma-tran.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Tổng Của Hai Ma Trận",
+  "body": " Tổng Của Hai Ma Trận  Text of section.  "
+},
+{
+  "id": "sec-nhan-ma-tran-voi-mot-so",
+  "level": "1",
+  "url": "sec-nhan-ma-tran-voi-mot-so.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Nhân Ma Trận Với Một Số",
+  "body": " Nhân Ma Trận Với Một Số  Text of section.  "
+},
+{
+  "id": "sec-nhan-hai-ma-tran",
+  "level": "1",
+  "url": "sec-nhan-hai-ma-tran.html",
+  "type": "Section",
+  "number": "3.3",
+  "title": "Nhân Hai Ma Trận",
+  "body": " Nhân Hai Ma Trận  Text of section.  "
+},
+{
+  "id": "sec-ma-tran-khoi",
+  "level": "1",
+  "url": "sec-ma-tran-khoi.html",
+  "type": "Section",
+  "number": "3.4",
+  "title": "Ma Trận Khối",
+  "body": " Ma Trận Khối  Text of section.  "
+},
+{
+  "id": "sec-nhan-ma-tran-theo-khoi",
+  "level": "1",
+  "url": "sec-nhan-ma-tran-theo-khoi.html",
+  "type": "Section",
+  "number": "3.5",
+  "title": "Nhân Ma Trận Theo Khối",
+  "body": " Nhân Ma Trận Theo Khối  Text of section.  "
+},
+{
+  "id": "sec-dinh-thuc",
+  "level": "1",
+  "url": "sec-dinh-thuc.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Định Thức",
+  "body": " Định Thức  Text of section.  "
+},
+{
+  "id": "sec-ma-tran-con-va-phan-phu-dai-so",
+  "level": "1",
+  "url": "sec-ma-tran-con-va-phan-phu-dai-so.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Ma Trận Con Và Phần Phụ Đại Số",
+  "body": " Ma Trận Con Và Phần Phụ Đại Số  Text of section.  "
+},
+{
+  "id": "sec-tinh-dinh-thuc",
+  "level": "1",
+  "url": "sec-tinh-dinh-thuc.html",
+  "type": "Section",
+  "number": "4.3",
+  "title": "Tính Định Thức",
+  "body": " Tính Định Thức  Text of section.  "
+},
+{
+  "id": "sec-phuong-phap-khu-gauss",
+  "level": "1",
+  "url": "sec-phuong-phap-khu-gauss.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Phương Pháp Khử Gauss",
+  "body": " Phương Pháp Khử Gauss  Text of section.  "
+},
+{
+  "id": "sec-phuong-phap-cramer-doi-voi-he-phuong-trinh-co-so-phuong-trinh-bang-so-an",
+  "level": "1",
+  "url": "sec-phuong-phap-cramer-doi-voi-he-phuong-trinh-co-so-phuong-trinh-bang-so-an.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Phương Pháp Cramer Đối Với Hệ Phương Trình Có Số Phương Trình Bằng Số Ẩn",
+  "body": " Phương Pháp Cramer Đối Với Hệ Phương Trình Có Số Phương Trình Bằng Số Ẩn  Text of section.  "
+},
+{
+  "id": "sec-mot-so-ung-dung",
+  "level": "1",
+  "url": "sec-mot-so-ung-dung.html",
+  "type": "Section",
+  "number": "5.3",
+  "title": "Một Số Ứng Dụng",
+  "body": " Một Số Ứng Dụng  Text of section.  "
+},
+{
+  "id": "sec-phan-tich-a-lu",
+  "level": "1",
+  "url": "sec-phan-tich-a-lu.html",
+  "type": "Section",
+  "number": "6.1",
+  "title": "Phân Tích A = LU",
+  "body": " Phân Tích A = LU  Text of section.  "
+},
+{
+  "id": "sec-phan-tich-a-cr",
+  "level": "1",
+  "url": "sec-phan-tich-a-cr.html",
+  "type": "Section",
+  "number": "6.2",
+  "title": "Phân Tích A = CR",
+  "body": " Phân Tích A = CR  Text of section.  "
+},
+{
+  "id": "sec-ma-tran-nghich-dao",
+  "level": "1",
+  "url": "sec-ma-tran-nghich-dao.html",
+  "type": "Section",
+  "number": "6.3",
+  "title": "Ma Trận Nghịch Đảo",
+  "body": " Ma Trận Nghịch Đảo  Text of section.  "
+},
+{
+  "id": "sec-ung-dung-de-giai-he-phuong-trinh-tuyen-tinh",
+  "level": "1",
+  "url": "sec-ung-dung-de-giai-he-phuong-trinh-tuyen-tinh.html",
+  "type": "Section",
+  "number": "6.4",
+  "title": "Ứng Dụng Để Giải Hệ Phương Trình Tuyến Tính",
+  "body": " Ứng Dụng Để Giải Hệ Phương Trình Tuyến Tính  Text of section.  "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
