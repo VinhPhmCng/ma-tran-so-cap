992b01fbe6482db9e4062a420914577f51eba7e6
VPC <vvvvvvvvvvvvvvvinh@gmail.com>
2023-12-26 18:44:30 +0700

    Deploy

---
+++ generated/trace/subsec-nhap-xuat-vector-2.js
@@ -0,0 +1,4 @@
+if (allTraceData === undefined) {
+ var allTraceData = {};
+ }
+ allTraceData[""] = {"code": "import numpy as np\n\n", "trace": [{"line": 1, "event": "step_line", "func_name": "<module>", "globals": {}, "ordered_globals": [], "stack_to_render": [], "heap": {}, "stdout": ""}, {"line": 1, "event": "exception", "func_name": "<module>", "globals": {}, "ordered_globals": [], "stack_to_render": [], "heap": {}, "stdout": "", "exception_msg": "ImportError: numpy not supported"}]}
